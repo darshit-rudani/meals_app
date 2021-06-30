6df0186855166f8f0cf4502d19bc5e9524560a02
Darshit Rudani <darshit112001@users.noreply.github.com>
2021-06-30 15:45:30 +0530

    Displaying Recipe Items & Using Network Images

--- lib/category_meal_screen.dart
+++ lib/category_meal_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:meals/meal_item.dart';
 import 'dummy_data.dart';
 
 class CategoryMealScreen extends StatelessWidget {
@@ -22,7 +23,13 @@ class CategoryMealScreen extends StatelessWidget {
       ),
       body: ListView.builder(
         itemBuilder: (ctx, index) {
-          return Text(categoryMeal[index].title);
+          return MealItem(
+            title: categoryMeal[index].title,
+            imageUrl: categoryMeal[index].imageUrl,
+            duration: categoryMeal[index].duration,
+            complexity: categoryMeal[index].complexity,
+            affordability: categoryMeal[index].affordability,
+          );
         },
         itemCount: categoryMeal.length,
       ),
---
+++ lib/meal_item.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'meal.dart';
+
+class MealItem extends StatelessWidget {
+  final String title;
+  final String imageUrl;
+  final int duration;
+  final Complexity complexity;
+  final Affordability affordability;
+
+  MealItem({
+    required this.title,
+    required this.imageUrl,
+    required this.duration,
+    required this.complexity,
+    required this.affordability,
+  });
+
+  void selectMeal() {}
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: selectMeal,
+      child: Card(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(15),
+        ),
+        elevation: 4,
+        margin: EdgeInsets.all(10),
+        child: Column(
+          children: [
+            Stack(
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(15),
+                    bottomRight: Radius.circular(15),
+                  ),
+                  child: Image.network(
+                    imageUrl,
+                    height: 250,
+                    width: double.infinity,
+                    fit: BoxFit.cover,
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
